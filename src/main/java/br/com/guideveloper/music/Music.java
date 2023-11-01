f84b70c71e994a82d9121fa6cf6202b32a251af7
guiper43 <guilhermemano321@gmail.com>
2023-11-01 08:47:21 -0300

    excluido helo word

--- src/main/java/br/com/guideveloper/music/Music.java
+++ src/main/java/br/com/guideveloper/music/Music.java
@@ -1,7 +1,9 @@
-    package br.com.guideveloper.music;
+package br.com.guideveloper.music;
 
 public class Music {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
+
 }
